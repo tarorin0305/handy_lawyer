6b58f02ae80c285fe5930e54ede28168ba7f1a4b
tarorin0305 <dayutakatarorin@gmail.com>
2021-01-06 01:59:56 +0900

    複数の項または号が存在する条文もパースできるよう修正

--- corporate_law/http_client.rb
+++ corporate_law/http_client.rb
@@ -2,6 +2,7 @@ require 'faraday'
 require "rexml/document"
 require 'active_support/all'
 require_relative '../lib/client/http_client'
+require 'pry-byebug'
 
 module CorporateLaw
   class Client
@@ -17,8 +18,32 @@ module CorporateLaw
 
     def parse_like_real_roppo(article)
       full_result = fetch_article_api(article).dig('DataRoot', 'ApplData', 'LawContents', 'Article')
-      output_to_stdout = "#{full_result.fetch('ArticleCaption')} #{full_result.fetch('ArticleTitle')}\n#{full_result.dig('Paragraph', 'ParagraphSentence', 'Sentence')}"
+      output_to_stdout = "#{full_result.fetch('ArticleCaption')} #{full_result.fetch('ArticleTitle')}\n#{parse_paragraph(full_result.fetch('Paragraph'))}"
       puts output_to_stdout
+    end
+
+    def parse_paragraph(paragraphs)
+      text = ''
+      if paragraphs.is_a?(Array)
+        paragraphs.each do |paragraph|
+          if paragraph.fetch('Num').to_i > 1
+            text << "  第#{paragraph.fetch('ParagraphNum')}項 " 
+          else
+            text << "  "
+          end
+          text << "#{paragraph.dig('ParagraphSentence', 'Sentence')}\n"
+          if paragraph.dig('Item').present?
+            paragraph.dig('Item').each do |item|
+              text << "    第#{item.fetch('ItemTitle')}号 #{item.dig('ItemSentence', 'Sentence')}\n"
+            end
+          end
+        end
+      else
+        text << '  '
+        text << paragraphs.dig('ParagraphSentence', 'Sentence')
+      end
+
+      text
     end
 
     def fetch_article_api(article)
