588be14103993a31cbfc43f43969e6742e0a5335
tarorin0305 <dayutakatarorin@gmail.com>
2021-01-07 23:22:25 +0900

    コマンドライン引数を複数取れるようにした

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require './corporate_law/http_client'
 
-article = ARGV[0].to_i
-CorporateLaw::Client.new.parse_like_real_roppo(article)
+ARGV.each do |article|
+  CorporateLaw::Client.new.parse_like_real_roppo(article.to_i)
+  puts "============================================" if ARGV.size > 1
+end
